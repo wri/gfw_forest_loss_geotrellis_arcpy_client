649deb9ef5572885ca758ada0ac39aecc575c3d3
Thomas Maschler <tmaschler@wri.org>
2019-06-25 16:35:27 +0200

    first attempt
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

---
+++ forest_loss_analysis/TreeCoverLossAnalysis.py
@@ -0,0 +1,509 @@
+import arcpy
+import binascii
+import boto3
+import itertools
+
+
+class TreeCoverLossAnalysis(object):
+    def __init__(self):
+        """Define the toolbox (the name of the toolbox is the name of the
+        .pyt file)."""
+        self.label = "Tree Cover Loss Analysis Toolbox"
+        self.alias = ""
+
+        # List of tool classes associated with this toolbox
+        self.tools = [Tool]
+
+
+class Tool(object):
+    def __init__(self):
+        """Define the tool (tool name is the name of the class)."""
+        self.label = "Tool"
+        self.description = ""
+        self.canRunInBackground = False
+
+    def getParameterInfo(self):
+        """Define parameter definitions"""
+        # First parameter
+        in_features = arcpy.Parameter(
+            displayName="Input Features",
+            name="in_features",
+            datatype="GPFeatureLayer",
+            parameterType="Required",
+            direction="Input",
+        )
+
+        tcd = arcpy.Parameter(
+            displayName="Tree Cover Density Threshold",
+            name="tcd",
+            datatype="GPLong",
+            parameterType="Required",
+            direction="Input",
+            default=[30],
+            multiValue=True,
+        )
+        # Set a value list of 1, 10 and 100
+
+        tcd.filter.type = "ValueList"
+        tcd.filter.list = list(range(0, 100, 5))
+
+        slack_user = arcpy.Parameter(
+            displayName="Slack user name",
+            name="slack_user",
+            datatype="GPString",
+            parameterType="Required",
+            direction="Input",
+        )
+
+        slack_user.filter.type = "ValueList"
+        slack_user.filter.list = ["David Gibbs", "liz.goldman", "thai", "thomas"]
+
+        nb_workers = arcpy.Parameter(
+            displayName="Number of workers",
+            name="nb_workers",
+            datatype="GPLong",
+            parameterType="Required",
+            direction="Input",
+            default=4,
+            category="Spark config",
+        )
+
+        instance_type = arcpy.Parameter(
+            displayName="Instance Parameter",
+            name="instance_parameter",
+            datatype="GPString",
+            parameterType="Required",
+            direction="Input",
+            default="m3.2xlarge",
+            category="Spark config",
+        )
+
+        tcd.filter.type = "ValueList"
+        tcd.filter.list = ["m3.2xlarge"]
+
+        params = [in_features, tcd, slack_user, nb_workers, instance_type]
+
+        return params
+
+    def isLicensed(self):
+        """Set whether tool is licensed to execute."""
+        return True
+
+    def updateParameters(self, parameters):
+        """Modify the values and properties of parameters before internal
+        validation is performed.  This method is called whenever a parameter
+        has been changed."""
+        return
+
+    def updateMessages(self, parameters):
+        """Modify the messages created by internal validation for each tool
+        parameter.  This method is called after internal validation."""
+        return
+
+    def execute(self, parameters, messages):
+        """The source code of the tool."""
+
+        in_features = parameters[0]
+
+        # Create fishnet
+        messages.addMessage("Compute in memory fishnet")
+        arcpy.CreateFishnet_management(
+            r"in_memory\fishnet",
+            "-180 -90",
+            "-180, 90",
+            1,
+            1,
+            180,
+            360,
+            labels="NO_LABELS",
+            template=arcpy.Extent(-180, -90, 180, 90),
+            geometry_type="POLYGON",
+        )
+
+        # intersect input feature with fishnet and forest loss extent
+
+        loss_extent = arcpy.AsShape(self.loss_extent, False)
+        sr = arcpy.SpatialReference(4326)
+        arcpy.DefineProjection_management(loss_extent, sr)
+
+        arcpy.Intersect_analysis(
+            [in_features, 2],
+            [r"in_memory\fishnet", 1],
+            [loss_extent, 1],
+            r"in_memory\out_features",
+            join_attributes="ONLY_FID",
+        )
+
+        # Export to WKB
+
+        with ("outfile.tsv", "a+") as output_file:
+
+            with arcpy.da.SearchCursor(
+                r"in_memory\out_features", ["OID@", "SHAPE@WKB"]
+            ) as cursor:
+                for row in cursor:
+                    gid = row[0]
+                    wkb = binascii.hexlify(row[1])
+                    output_file.write(gid + "\t" + wkb + "\n")
+
+        # Upload inout features to S3
+
+        s3 = boto3.resource("s3")
+        s3.meta.client.upload_file(
+            output_file, "gfw-files", "2018_updates/csv", "input_features.tsv"
+        )
+
+        # config spark cluster
+        # start job
+        self._laucn_emr
+
+        return
+
+    def _launch_emr(self, instance_type, instance_count, in_features, tcd):
+
+        client = boto3.client("emr")
+        response = client.run_job_flow(
+            Name="Geotrellis Forest Loss Analysis",
+            LogUri="s3://gfw-files/2018_update/spark/logs",
+            ReleaseLabel="emr-5.24.0",
+            Instances={
+                # "MasterInstanceType": instance_type,
+                # "SlaveInstanceType": instance_type,
+                # "InstanceCount": instance_count + 1,
+                "InstanceGroups": [
+                    {
+                        "Name": "geotrellis-treecoverloss-master",
+                        "Market": "ON_DEMAND",
+                        "InstanceRole": "MASTER",
+                        "InstanceType": instance_type,
+                        "InstanceCount": 1,
+                        "Configurations": [
+                            {
+                                "Classification": "string",
+                                "Configurations": {"... recursive ..."},
+                                "Properties": {"string": "string"},
+                            }
+                        ],
+                        "EbsConfiguration": {
+                            "EbsBlockDeviceConfigs": [
+                                {
+                                    "VolumeSpecification": {
+                                        "VolumeType": "gp2",
+                                        "SizeInGB": 30,
+                                    },
+                                    "VolumesPerInstance": 1,
+                                }
+                            ],
+                            "EbsOptimized": True,
+                        },
+                    },
+                    {
+                        "Name": "geotrellis-treecoverloss-cores",
+                        "Market": "SPOT",
+                        "InstanceRole": "CORE",
+                        "BidPrice": None,
+                        "InstanceType": instance_type,
+                        "InstanceCount": instance_count,
+                        "EbsConfiguration": {
+                            "EbsBlockDeviceConfigs": [
+                                {
+                                    "VolumeSpecification": {
+                                        "VolumeType": "gp2",
+                                        "SizeInGB": 30,
+                                    },
+                                    "VolumesPerInstance": 1,
+                                }
+                            ],
+                            "EbsOptimized": True,
+                        },
+                    },
+                ],
+                "Ec2KeyName": "tmaschler2_wri",
+                "Placement": {"AvailabilityZone": "us-east-1c"},
+                "KeepJobFlowAliveWhenNoSteps": False,
+                "TerminationProtected": False,
+                "HadoopVersion": "string",
+                "Ec2SubnetId": "string",
+                "Ec2SubnetIds": ["subnet-08458452c1d05713b"],
+                "EmrManagedMasterSecurityGroup": "subnet-08458452c1d05713b",
+                "EmrManagedSlaveSecurityGroup": "subnet-08458452c1d05713b",
+                "ServiceAccessSecurityGroup": "string",
+                "AdditionalMasterSecurityGroups": [
+                    "sg-d76cdbc1",
+                    "sg-11e40a60",
+                    "subnet-08458452c1d05713b",
+                ],
+                "AdditionalSlaveSecurityGroups": ["subnet-08458452c1d05713b"],
+            },
+            Steps=[
+                {
+                    "Name": "treecoverloss-analysis",
+                    "ActionOnFailure": "TERMINATE_CLUSTER",
+                    "HadoopJarStep": {
+                        "Properties": [{"Key": "string", "Value": "string"}],
+                        "Jar": "s3://gfw-files/2018_update/spark/jars/treecoverloss-assembly-0.8.2.jar",
+                        "MainClass": "org.globalforestwatch.treecoverloss.TreeCoverLossSummaryMain",
+                        "Args": [
+                            "--features",
+                            in_features,
+                            "--output s3://gfw-files/2018_update/results",
+                        ]
+                        + list(zip(itertools.repeat("--tcd"), [i for i in tcd])),
+                    },
+                }
+            ],
+            Applications=[
+                {"Name": "Spark", "Version": "2.4.2"},
+                {"Name": "Zeppelin", "Version": "0.8.1"},
+                {"Name": "Ganglia", "Version": "3.7.2"},
+            ],
+            Configurations=[
+                {
+                    "classification": "spark",
+                    "properties": {"maximizeResourceAllocation": "true"},
+                    "configurations": [],
+                },
+                {
+                    "classification": "spark-defaults",
+                    "properties": {
+                        "spark.executor.memory": "3G",
+                        "spark.driver.memory": "3G",
+                        "spark.driver.cores": "1",
+                        "spark.driver.maxResultSize": "3G",
+                        "spark.rdd.compress": "true",
+                        "spark.executor.cores": "1",
+                        "spark.sql.shuffle.partitions": "13390",
+                        "spark.shuffle.spill.compress": "true",
+                        "spark.shuffle.compress": "true",
+                        "spark.default.parallelism": "13390",
+                        "spark.shuffle.service.enabled": "true",
+                        "spark.executor.extraJavaOptions": "-XX:+UseParallelGC -XX:+UseParallelOldGC -XX:OnOutOfMemoryError='kill -9 %p'",
+                        "spark.executor.instances": "1339",
+                        "spark.yarn.executor.memoryOverhead": "1G",
+                        "spark.dynamicAllocation.enabled": "false",
+                        "spark.driver.extraJavaOptions": "-XX:+UseParallelGC -XX:+UseParallelOldGC -XX:OnOutOfMemoryError='kill -9 %p'",
+                    },
+                    "configurations": [],
+                },
+                {
+                    "classification": "yarn-site",
+                    "properties": {
+                        "yarn.nodemanager.pmem-check-enabled": "false",
+                        "yarn.resourcemanager.am.max-attempts": "1",
+                        "yarn.nodemanager.vmem-check-enabled": "false",
+                    },
+                    "configurations": [],
+                },
+            ],
+            VisibleToAllUsers=True,
+            JobFlowRole="EMR_EC2_DefaultRole",
+            Tags=[
+                {"Key": "Project", "Value": "Global Forest Watch"},
+                {"Key": "Job", "Value": "Tree Cover Loss Analysis"},
+            ],
+        )
+        return response
+
+    loss_extent = {
+        "type": "MultiPolygon",
+        "coordinates": [
+            [
+                [
+                    [160, -50],
+                    [160, -40],
+                    [170, -40],
+                    [170, -30],
+                    [180, -30],
+                    [180, -40],
+                    [180, -50],
+                    [170, -50],
+                    [160, -50],
+                ]
+            ],
+            [
+                [
+                    [-160, 10],
+                    [-160, 20],
+                    [-170, 20],
+                    [-170, 30],
+                    [-160, 30],
+                    [-150, 30],
+                    [-150, 20],
+                    [-150, 10],
+                    [-160, 10],
+                ]
+            ],
+            [
+                [
+                    [140, -50],
+                    [140, -40],
+                    [130, -40],
+                    [120, -40],
+                    [110, -40],
+                    [110, -30],
+                    [110, -20],
+                    [110, -10],
+                    [100, -10],
+                    [90, -10],
+                    [90, 0],
+                    [80, 0],
+                    [70, 0],
+                    [70, 10],
+                    [70, 20],
+                    [60, 20],
+                    [60, 10],
+                    [60, 0],
+                    [50, 0],
+                    [50, -10],
+                    [60, -10],
+                    [60, -20],
+                    [60, -30],
+                    [50, -30],
+                    [40, -30],
+                    [40, -40],
+                    [30, -40],
+                    [20, -40],
+                    [10, -40],
+                    [10, -30],
+                    [10, -20],
+                    [10, -10],
+                    [0, -10],
+                    [0, 0],
+                    [-10, 0],
+                    [-20, 0],
+                    [-20, 10],
+                    [-30, 10],
+                    [-30, 20],
+                    [-20, 20],
+                    [-20, 30],
+                    [-20, 40],
+                    [-10, 40],
+                    [-10, 50],
+                    [-20, 50],
+                    [-20, 60],
+                    [-30, 60],
+                    [-30, 70],
+                    [-20, 70],
+                    [-10, 70],
+                    [0, 70],
+                    [10, 70],
+                    [10, 80],
+                    [20, 80],
+                    [30, 80],
+                    [40, 80],
+                    [40, 70],
+                    [50, 70],
+                    [50, 80],
+                    [60, 80],
+                    [70, 80],
+                    [80, 80],
+                    [90, 80],
+                    [100, 80],
+                    [110, 80],
+                    [120, 80],
+                    [130, 80],
+                    [140, 80],
+                    [150, 80],
+                    [160, 80],
+                    [170, 80],
+                    [180, 80],
+                    [180, 70],
+                    [180, 60],
+                    [180, 50],
+                    [170, 50],
+                    [160, 50],
+                    [160, 40],
+                    [150, 40],
+                    [150, 30],
+                    [140, 30],
+                    [140, 20],
+                    [130, 20],
+                    [130, 10],
+                    [140, 10],
+                    [140, 0],
+                    [150, 0],
+                    [160, 0],
+                    [170, 0],
+                    [170, -10],
+                    [180, -10],
+                    [180, -20],
+                    [170, -20],
+                    [170, -30],
+                    [160, -30],
+                    [160, -40],
+                    [150, -40],
+                    [150, -50],
+                    [140, -50],
+                ]
+            ],
+            [
+                [
+                    [-80, -60],
+                    [-80, -50],
+                    [-80, -40],
+                    [-80, -30],
+                    [-80, -20],
+                    [-80, -10],
+                    [-90, -10],
+                    [-100, -10],
+                    [-100, 0],
+                    [-100, 10],
+                    [-110, 10],
+                    [-110, 20],
+                    [-120, 20],
+                    [-120, 30],
+                    [-130, 30],
+                    [-130, 40],
+                    [-130, 50],
+                    [-140, 50],
+                    [-150, 50],
+                    [-160, 50],
+                    [-170, 50],
+                    [-180, 50],
+                    [-180, 60],
+                    [-180, 70],
+                    [-170, 70],
+                    [-170, 80],
+                    [-160, 80],
+                    [-150, 80],
+                    [-140, 80],
+                    [-130, 80],
+                    [-120, 80],
+                    [-110, 80],
+                    [-100, 80],
+                    [-90, 80],
+                    [-80, 80],
+                    [-70, 80],
+                    [-60, 80],
+                    [-60, 70],
+                    [-60, 60],
+                    [-50, 60],
+                    [-50, 50],
+                    [-50, 40],
+                    [-60, 40],
+                    [-60, 30],
+                    [-70, 30],
+                    [-70, 20],
+                    [-60, 20],
+                    [-50, 20],
+                    [-50, 10],
+                    [-40, 10],
+                    [-40, 0],
+                    [-30, 0],
+                    [-30, -10],
+                    [-30, -20],
+                    [-40, -20],
+                    [-40, -30],
+                    [-50, -30],
+                    [-50, -40],
+                    [-60, -40],
+                    [-60, -50],
+                    [-50, -50],
+                    [-50, -60],
+                    [-60, -60],
+                    [-70, -60],
+                    [-80, -60],
+                ]
+            ],
+        ],
+    }
